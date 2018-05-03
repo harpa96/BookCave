119c26828cdace030a2e5922fa77c69caf1add41
Vigdís Birna Þorsteinsdóttir <vigdis16@ru.is>
2018-05-03 13:11:19 +0000

    merging with develop to get gitignore

--- Models/EntityModels/Book.cs
+++ Models/EntityModels/Book.cs
@@ -4,6 +4,22 @@ namespace BookCave.Models.EntityModels
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string AuthorId {get; set;}
+        
+        public int Date {get; set;}
+
+        public string Genre {get; set;}
+
+        public string Language {get; set;}
+
+        public float Rating {get; set;}
+
+        public int Price {get; set;}
+
+        public string Image {get; set;}
+
+        public string Description {get; set;}
     
     }
 }
