c7d5f483a0e7a7726e7d345331f76a0578eab1d1
dagrunosk <dagrun17@ru.is>
2018-05-06 21:18:25 +0000

    meira

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using BookCave.Models;
 using BookCave.Services;
 using BookCave.Models.ViewModels;
