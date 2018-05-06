e79ce1f99ed5e848a0bcfe2e049c9e439020bc59
dagrunosk <dagrun17@ru.is>
2018-05-06 19:37:53 +0000

    LogOut

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -44,6 +44,44 @@ public class AccountController : Controller
             //The user is successfully registered
             //Add the concatenated first and last name as fullName in claims
             await _userManager.AddClaimAsync(user, new Claim("Name", $"{model.FirstName} {model.LastName}"));
+            await _signInManager.SignInAsync(user, false);
+
+            return RedirectToAction("Index", "Home");
         }
+
+        return View();
     }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+        if (result.Succeeded)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> LogOut()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Login", "Account");
+    }
+
 }
