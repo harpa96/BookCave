1f93fae4f87778e797d7d3464a79b299b3ba9f80
Vigdís Birna Þorsteinsdóttir <vigdis16@ru.is>
2018-05-07 13:48:09 +0000

    working on rating

---
+++ Models/InputModels/RatingInputModel.cs
@@ -0,0 +1,10 @@
+namespace BookCave.Models.InputModels
+{
+    public class RatingInputModel
+    {
+        public int Id {get; set;}
+        public int BookId {get; set;}
+
+        public float Rate {get; set;}
+    }
+}
