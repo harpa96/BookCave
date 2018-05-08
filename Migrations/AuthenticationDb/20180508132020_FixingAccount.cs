f71dbc89b40679b9dce1f599a9b45336cf330343
dagrunosk <dagrun17@ru.is>
2018-05-08 13:39:15 +0000

    laga meira

---
+++ Migrations/AuthenticationDb/20180508132020_FixingAccount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations.AuthenticationDb
+{
+    public partial class FixingAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Identity",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Identity",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
