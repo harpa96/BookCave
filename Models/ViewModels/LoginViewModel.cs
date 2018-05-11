7710c04b21b0d01cf09600810a4fa73585c48beb
Harpa Hreinsdottir <harpah16@ru.is>
2018-05-11 15:36:58 +0000

    homecontroller fixing

--- Models/ViewModels/LoginViewModel.cs
+++ Models/ViewModels/LoginViewModel.cs
@@ -4,6 +4,7 @@ namespace BookCave.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [RegularExpression("/^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$/", ErrorMessage = "Email has to be on the format example@example.example")]
         [Required(ErrorMessage= "Email is required")]
         public string Email { get; set; }
 
